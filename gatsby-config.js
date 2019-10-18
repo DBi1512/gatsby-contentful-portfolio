df81d4088b156d667e6628213137cd3cd65fd027
Dean Phan <dean.t.phan@gmail.com>
2019-10-18 13:04:46 +0300

    fixed Environment in Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         {
             resolve: "gatsby-source-contentful",
             options: {
-                spaceId: "zan1d5v6n5j1",
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
                 // Learn about environment variables: https://gatsby.dev/env-vars
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
             },
